3e2f0a0d6f21a01a0f52b22a9deaa6e1e4e5782a
admin <515769618@qq.com>
2017-01-13 17:05:09 +0800

    ACache更新

---
+++ app/src/main/java/demo/zxg/com/myapplication/AboutActivity.java
@@ -0,0 +1,15 @@
+package demo.zxg.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+
+	}
+
+}
--- app/src/main/java/demo/zxg/com/myapplication/MainActivity.java
+++ app/src/main/java/demo/zxg/com/myapplication/MainActivity.java
@@ -1,13 +1,58 @@
 package demo.zxg.com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+/**
+ * 
+ * @ClassName: MainActivity
+ * @Description: 主界面
+ * @Author Yoson Hao
+ * @WebSite www.haoyuexing.cn
+ * @Email haoyuexing@gmail.com
+ * @Date 2013-8-8 下午2:08:47
+ * 
+ */
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void string(View v) {
+		startActivity(new Intent().setClass(this, SaveStringActivity.class));
+	}
+
+	public void jsonobject(View v) {
+		startActivity(new Intent().setClass(this, SaveJsonObjectActivity.class));
+	}
+
+	public void jsonarray(View v) {
+		startActivity(new Intent().setClass(this, SaveJsonArrayActivity.class));
+	}
+
+	public void bitmap(View v) {
+		startActivity(new Intent().setClass(this, SaveBitmapActivity.class));
+	}
+
+    public void media(View v) {
+        startActivity(new Intent().setClass(this, SaveMediaActivity.class));
     }
+
+	public void drawable(View v) {
+		startActivity(new Intent().setClass(this, SaveDrawableActivity.class));
+	}
+
+	public void object(View v) {
+		startActivity(new Intent().setClass(this, SaveObjectActivity.class));
+	}
+
+	public void about(View v) {
+		startActivity(new Intent().setClass(this, AboutActivity.class));
+	}
+
 }
